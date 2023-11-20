1fa799b29c719d52d2ad7e5f358894ab0c1ddcae
Desmond Ho <desmondh268@gmail.com>
2023-11-20 15:43:30 -0800

    added bandit security

--- vars/microservices.groovy
+++ vars/microservices.groovy
@@ -5,11 +5,18 @@ def call() {
         stage('Build') {
             steps {
                 sh 'pip install -r requirements.txt --break-system-packages'
+                sh 'pip install bandit --break-system-packages'
             }
         }
         stage("Python Lint") {
             steps {
                 sh 'find . -type f -name "*.py" | xargs pylint --fail-under=5.0'
+            }
+        }
+
+        stage("Python Security Scanning") {
+            steps {
+                sh 'find . -type f -name "*.py" | xargs bandit'
             }
         }
 
