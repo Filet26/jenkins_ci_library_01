29b7a67e77f8c3ec8a83a6968d318d40e2b6e016
Desmond Ho <desmondh268@gmail.com>
2023-11-20 15:14:19 -0800

    added build step

--- vars/microservices.groovy
+++ vars/microservices.groovy
@@ -2,11 +2,11 @@ def call() {
     pipeline {
     agent any
     stages {
-        // stage('Build') {
-        //     steps {
-        //         sh 'echo haha'
-        //     }
-        // }
+        stage('Build') {
+            steps {
+                sh 'pip install -r requirements.txt --break-system-packages'
+            }
+        }
         stage("Python Lint") {
             steps {
                 sh 'find . -type f -name "*.py" | xargs pylint --fail-under=5.0'
