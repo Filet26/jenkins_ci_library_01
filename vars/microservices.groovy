3f66c8b6a85081effedfa4fd13e78abba7c11d51
Desmond Ho <desmondh268@gmail.com>
2023-11-20 15:12:02 -0800

    test push

--- vars/microservices.groovy
+++ vars/microservices.groovy
@@ -2,11 +2,17 @@ def call() {
     pipeline {
     agent any
     stages {
-        stage('Build') {
+        // stage('Build') {
+        //     steps {
+        //         sh 'echo haha'
+        //     }
+        // }
+        stage("Python Lint") {
             steps {
-                sh 'echo haha'
+                sh 'find . -type f -name "*.py" | xargs pylint --fail-under=5.0'
             }
         }
+
 
     }
 
